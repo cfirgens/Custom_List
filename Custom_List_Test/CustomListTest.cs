eafa07440b06ddd34a871b5b0b55562f9521c7f1
Chris Firgens <firgenscj@gmail.com>
2019-07-18 08:53:58 -0500

    added two remove test methods

--- Custom_List_Test/CustomListTest.cs
+++ Custom_List_Test/CustomListTest.cs
@@ -104,6 +104,68 @@ namespace Custom_List_Test
         }
 
 
+        //if i remove something to a customlist who has items in the array, it should shift items over
+        [TestMethod]
+        public void Remove_RemoveFromPopulatedArray_ShiftEverythingOver()
+        {
+            // arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 3;
+            int actual;
+
+            // act
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Add(4);
+            test.Remove[1];
+
+            actual = test[1];
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //if i remove something to a customlist who has items in the array and the capacity can be lowered, it should lower capacity
+        [TestMethod]
+        public void Remove_RemoveFromPopulatedArrayToLowerCapacity_LowerCapacity()
+        {
+            // arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 4;
+            int actual;
+
+            // act
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Add(4);
+            test.Add(5);
+            test.Remove[0];
+
+            actual = test.Capacity;
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //if i remove something to a customlist who has items in the array the count should lower
+        [TestMethod]
+        public void Remove_RemoveFromPopulatedArray_CountLowers()
+        {
+            // arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 2;
+            int actual;
+
+            // act
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Remove[1];
+
+            actual = test.Count;
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
 
     }
 }
